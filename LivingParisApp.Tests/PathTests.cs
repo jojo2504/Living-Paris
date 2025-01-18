c2240f377d810eabf5d1dc71ba1a975db7ac2d1b
jojo2504 <jojo.duchuy@gmail.com>
2025-01-18 17:57:08 +0100

    feat(TestUnit): tested if appsettings.json exists in config directory
    This test purpose was for checking if the solution directory finder was working

---
+++ LivingParisApp.Tests/PathTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+using LivingParisApp.Services.EnvironmentSetup;
+
+namespace LivingParisApp.Tests;
+
+public class PathTests {
+    private readonly ITestOutputHelper _output;
+
+    public PathTests(ITestOutputHelper output) {
+        _output = output;
+    }
+
+    [Fact]
+    public void AppSettings_Should_Exist_In_Config_Directory() {
+        string solutionRoot = Constants.GetSolutionDirectoryInfo().FullName;
+        string appSettingsPath = Path.Combine(solutionRoot, "config", "appsettings.json");
+        Assert.True(File.Exists(appSettingsPath), $"The file {appSettingsPath} does not exist.");
+    }
+}
+
+
