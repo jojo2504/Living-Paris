73583aed5bc86661ecc85e8dd42f63b981ac65da
jojo2504 <jojo.duchuy@gmail.com>
2025-03-31 13:11:24 +0200

    fix: ShortestPathEngine can now accept a start node

--- LivingParisApp.Core/Engines/PathFinding/BellmanFord.cs
+++ LivingParisApp.Core/Engines/PathFinding/BellmanFord.cs
@@ -3,7 +3,7 @@ using LivingParisApp.Core.Mapping;
 
 namespace LivingParisApp.Core.Engines.ShortestPaths{
     public class BellmanFord<T> : ShortestPathsEngine<T> where T : IStation {
-        public override void Init(Map<T> map) {
+        public override void Init(Map<T> map, T? first = default) {
             throw new NotImplementedException();
         }
 
